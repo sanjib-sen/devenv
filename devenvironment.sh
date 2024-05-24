0de51c2b61d63ed3a9d5442c0be72967d150b8aa
Sanjib Kumar Sen <sksenonline@gmail.com>
2024-05-24 06:34:05 +0000

    Update

--- devenvironment.sh
+++ devenvironment.sh
@@ -1,14 +1,7 @@
 # Here you can add your automation scripts to download compilers, build tools, etc.
 # Note that you must add non-interactive version of the commands where no stdin input or interaction is needed
-# Here I have added some build tools for popular languages
-
+# Here I have added one for rust
 
 # Download and Install Rust
 # curl https://sh.rustup.rs -sSf | sh -s -- -y && . "$HOME/.cargo/env"
-
-# Download and install bun.js
-# curl -fsSL https://bun.sh/install | bash
-
-# Download and Install Nodejs via NVM
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash && source ~/.zshrc && nvm install --latest-npm
 
